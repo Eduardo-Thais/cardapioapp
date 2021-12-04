0cba874fac5d82cdd5b617b1fdaef4466ef41ab0
Eduardo de Souza Santos Thais <dudu2541999@gmail.com>
2021-12-04 12:04:08 -0300

    Update DataConfiguration.java

--- src/main/java/com/cardapioapp/cardapioapp/DataConfiguration.java
+++ src/main/java/com/cardapioapp/cardapioapp/DataConfiguration.java
@@ -16,25 +16,25 @@ public class DataConfiguration {
 	@Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl(System.getenv("JDBC_DATABASE_URL"));
-        //dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        //dataSource.setUrl("jdbc:mysql://localhost:3306/cardapiodb");
-        //dataSource.setUsername("root");
-        //dataSource.setPassword("");
+        //dataSource.setDriverClassName("org.postgresql.Driver");
+        //dataSource.setUrl(System.getenv("JDBC_DATABASE_URL"));
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/cardapiodb");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
         return dataSource;
     }
 	
 	@Bean
 	public JpaVendorAdapter jpaVendorAdapter(){
 		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-		adapter.setDatabase(Database.POSTGRESQL);
-		//adapter.setDatabase(Database.MYSQL);
+		//adapter.setDatabase(Database.POSTGRESQL);
+		adapter.setDatabase(Database.MYSQL);
 		adapter.setShowSql(true);
 		adapter.setGenerateDdl(true);
 		//adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
-		//adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5InnoDBDialect");
-		adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5InnoDBDialect");
+		//adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
 		adapter.setPrepareConnection(true);
 		return adapter;
 	}
